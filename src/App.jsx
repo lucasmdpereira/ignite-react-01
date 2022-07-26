c91cab3a277cdd927dd7a797fc13ba929ec19c59
Lucas Pereira <lucasmdpereira@gmail.com>
2022-07-26 16:05:06 -0300

    :construction: feat: CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import './styles.css'
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, distinctio sunt? Aspernatur suscipit nisi facere tenetur soluta, laboriosam temporibus quasi ea quod blanditiis commodi sapiente deserunt illo explicabo numquam odio! "
