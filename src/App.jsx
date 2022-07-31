d591fefa2423fe2a3ef2602e784f01fc7244b533
Lucas Pereira <lucasmdpereira@gmail.com>
2022-07-31 14:55:21 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
     <div>
       <Header />
       
-      <Post 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, distinctio sunt? Aspernatur suscipit nisi facere tenetur soluta, laboriosam temporibus quasi ea quod blanditiis commodi sapiente deserunt illo explicabo numquam odio! "
       />
@@ -17,6 +22,8 @@ function App() {
       />
       <Post />
       <Post />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+    return(
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1659205619507-e3892b32947a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=10" alt="" />
+        
+            <div className={styles.profile}>
+                <strong>Diego Fernandes</strong>
+                <span>Web Developer</span> 
+            </div>
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
