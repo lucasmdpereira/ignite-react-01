76f916b405f338e299ceedebf3c7c0bcf54f4fc3
Lucas Pereira <lucasmdpereira@gmail.com>
2022-08-09 15:11:37 -0300

    Os motores do React - Inserindo comentários (Programação declarativa)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,11 @@
-import styles from './Comment.module.css';
-
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
-
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
@@ -15,13 +13,12 @@ export function Comment() {
               <strong>Diego Fernandes</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
-
             <button title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,16 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { useState } from 'react';
-
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
-
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
+    'Post muito bacana, hein?!'
   ]);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -20,11 +19,15 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true
   });
-
   function handleCrateNewComment() {
     event.preventDefault()
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -50,13 +53,16 @@ export function Post({ author, publishedAt, content }) {
           }
         })}
       </div>
-
       <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
+
         <footer>
           <button type="submit">Publicar</button>
         </footer>
@@ -64,7 +70,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
