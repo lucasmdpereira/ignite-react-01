5005825eb271bf5b689f647863d591b60bd78ee3
Lucas Pereira <lucasmdpereira@gmail.com>
2022-08-23 11:50:32 -0300

    Os motores do React - Entendendo closures no React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,17 +1,17 @@
+import { useState } from "react";
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
-import { useState } from 'react';
-
 export function Comment({ content, onDeleteComment }) {
-  let [likeCount, setLikeCount] = useState(0);
-
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComment() {
     onDeleteComment(content);
   }
 
-  function handleLikeComment(){
-    setLikeCount(likeCount++);
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
@@ -24,7 +24,6 @@ export function Comment({ content, onDeleteComment }) {
               <strong>Diego Fernandes</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
-
             <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
@@ -32,7 +31,7 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button onClick={(handleLikeComment)}>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
             Aplaudir <span>{likeCount}</span>
           </button>
